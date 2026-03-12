7c3012989683ba5349d70f713045739bf31b6229
Your Name <zzzpppaaalllqqqppp@gmail.com>
2026-03-13 00:49:02 +0900

    fix: deploy.shのNFCフォルダ同期を無効化（古いファイルで上書きされる問題を修正）

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 GIT_FOLDER="/Users/fukitakatsumi/Desktop/スキル/ゴルフゲーム"
 
-python3 << 'PYEOF'
-import os, shutil, unicodedata
-
-git_folder = '/Users/fukitakatsumi/Desktop/スキル/ゴルフゲーム'
-parent = '/Users/fukitakatsumi/Desktop/スキル'
-
-for item in os.listdir(parent):
-    if unicodedata.normalize('NFC', item) == 'ゴルフゲーム':
-        full_path = os.path.join(parent, item)
-        if os.path.realpath(full_path) != os.path.realpath(git_folder):
-            print(f'NFCフォルダ検出: {full_path}')
-            for f in os.listdir(full_path):
-                src = os.path.join(full_path, f)
-                dst = os.path.join(git_folder, f)
-                if os.path.isfile(src):
-                    try:
-                        shutil.copy2(src, dst)
-                        print(f'  同期: {f}')
-                    except Exception as e:
-                        pass  # 同一ファイルエラーは無視して続行
-PYEOF
+# ※ NFCフォルダ同期を無効化（macOSのUnicode正規化の違いで
+#    別フォルダと誤判定し、古いファイルで上書きされる問題を回避）
+# 編集はこのフォルダで行い、そのままデプロイしてください。
 
 cd "$GIT_FOLDER"
 git add -A
